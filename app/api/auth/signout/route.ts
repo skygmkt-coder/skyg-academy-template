edebff30e4d206b71af298ac8ae270bdb00938f6
skygmkt-coder <skyg.mkt@gmail.com>
2026-05-12 21:12:32 -0600

    Update route.ts

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -14,7 +14,7 @@ export async function POST() {
           return cookieStore.getAll();
         },
 
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(
               ({ name, value, options }) =>
