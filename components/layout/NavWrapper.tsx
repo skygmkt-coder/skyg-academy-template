e732e426e6a950782109ec4a90f32a0492e9b8e3
skygmkt-coder <skyg.mkt@gmail.com>
2026-05-13 10:18:45 -0600

    Update NavWrapper.tsx

--- components/layout/NavWrapper.tsx
+++ components/layout/NavWrapper.tsx
@@ -1,36 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
-interface NavWrapperProps {
-  nav: React.ReactNode;
+export default function NavWrapper({
+  children,
+}: {
   children: React.ReactNode;
-}
-
-// Rutas que tienen su propio layout completo (sin nav global)
-const EXCLUDED = ["/admin", "/learn"];
-
-export default function NavWrapper({ nav, children }: NavWrapperProps) {
-  const pathname = usePathname();
-  const excluded = EXCLUDED.some(p => pathname.startsWith(p));
-
-  if (excluded) {
-    return <>{children}</>;
-  }
-
-  return (
-    <>
-      {nav}
-      <div
-        className="min-h-screen"
-        style={{ paddingLeft: "var(--sidebar-offset, 0px)" }}
-      >
-        {children}
-      </div>
-      <style>{`
-        @media (min-width: 768px) { :root { --sidebar-offset: 68px; } }
-        @media (max-width: 767px) { :root { --sidebar-offset: 0px; } }
-      `}</style>
-    </>
-  );
+}) {
+  return <>{children}</>;
 }
